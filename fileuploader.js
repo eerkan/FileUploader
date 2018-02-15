3c9d6036542fef9e03efc7587452028575cb8268
eerkan <emreerkanbilgi2@gmail.com>
2018-02-15 14:31:20 +0300

    Update fileuploader.js

--- fileuploader.js
+++ fileuploader.js
@@ -196,14 +196,5 @@ class FileUploader{
             'type': this.file.type
         }
     }
-
-    str2ab(str) {
-        var buf = new ArrayBuffer(str.length);
-        var bufView = new Uint8Array(buf);
-        for (var i=0, strLen=str.length; i < strLen; i++) {
-            bufView[i] = str.charCodeAt(i);
-        }
-        return buf;
-    }
     
 };
